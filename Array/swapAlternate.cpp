fec479af2bcfdcccf0416a84760d30c960f2c2b0
Daksh Yadav <dakshrao1111@gmail.com>
2024-11-07 21:52:48 +0530

    Arrays

---
+++ Array/swapAlternate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternateElements(int arr[], int size) {
+    for (int i = 0; i < size - 1; i += 2) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array -> ";
+    printArray(arr, size);
+
+    swapAlternateElements(arr, size);
+
+    cout << "Array after swap -> ";
+    printArray(arr, size);
+
+    return 0;
+}
