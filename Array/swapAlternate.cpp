83531cb61ed4d25dd33793bc62ad251f91ae0c48
Daksh Yadav <dakshrao1111@gmail.com>
2024-11-08 21:46:00 +0530

    Arrays

--- Array/swapAlternate.cpp
+++ Array/swapAlternate.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 void swapAlternateElements(int arr[], int size) {
-    for (int i = 0; i < size - 1; i += 2) {
-        int temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
+    for (int i = 0; i < size; i += 2) {
+        if(arr[i+1] < size){
+            swap(arr[i], arr[i+1]);
+        }
     }
 }
 
