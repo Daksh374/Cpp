a2543f9a99128b6bcef81b3ed0eac850e78d9408
Daksh Yadav <dakshrao1111@gmail.com>
2025-06-19 12:11:03 +0530

    priority queuq

--- STL/Priority_queue.cpp
+++ STL/Priority_queue.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 void explainPQ(){
     priority_queue<int> pq;
-    pq.push(2);
+    pq.push(2);                           // PUSH AND POP TC => o(logn)
     pq.push(4);
     pq.push(10);
     pq.push(6);  //{10,6,4,2}
 
-    cout << pq.top();  //10
+    cout << pq.top();  //10      // TC => O(1)
 
     pq.top();   //{6,4,2}
 
