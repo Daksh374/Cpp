49db60087934e5089def1200bf82ded1c481301e
Daksh Yadav <dakshrao1111@gmail.com>
2025-06-19 12:09:32 +0530

    Priority Queue

---
+++ STL/Priority_queue.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <queue>
+using namespace std;
+
+// IN PQ, THE LARGEST ELEMENT IS STORED AT THE TOP , ELEMENTS ARE NOT ADDED LINEARLY
+// IT FOLLOWS A TREE STRUCTURE TO STORE THE ELEMENTS IN THE QUEUE
+
+void explainPQ(){
+    priority_queue<int> pq;
+    pq.push(2);
+    pq.push(4);
+    pq.push(10);
+    pq.push(6);  //{10,6,4,2}
+
+    cout << pq.top();  //10
+
+    pq.top();   //{6,4,2}
+
+    //  SIZE AND SWAP FUNCTIONS ARE SAME AS OTHERS
+
+    // FOR MINIMUM HEAP
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    pq.push(2);
+    pq.push(4);
+    pq.push(3);
+    pq.push(8);  //{2,3,4,8}
+}
