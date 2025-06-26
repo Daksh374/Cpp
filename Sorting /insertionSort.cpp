efa04898bfca852f9e16178ba1ef7bf9d1c0fe3b
Daksh Yadav <dakshrao1111@gmail.com>
2025-06-26 12:50:40 +0530

    insertion sort

---
+++ Sorting /insertionSort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[] , int n){
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void insertion_sort(int arr[], int n){
+    for(int i = 0 ; i < n ; i++){
+        int j = i;
+        while(j>0 && arr[j-1] > arr[j]){        //j >0 && left > right
+            swap(arr[j-1], arr[j]);
+            j--;
+        }
+    }
+    
+}
+
+int main(){
+    int n;
+    cout << "Enter length of Array -> ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Array before sorting -> ";
+    printArray(arr , n);
+
+    cout << "Array after sorting -> ";
+    insertion_sort(arr , n);
+    printArray(arr , n);
+
+    return 0;
+
+}
+
+// TC -> 
+// TC -> 
